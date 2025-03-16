9228e238592ea9189db3003c0f971b388b61b5b0
Mami-Tomoe <se7enclanonline@gmail.com>
2025-03-16 19:28:10 +0200

    Added Reset Score

---
+++ Reset Score/reset_score.lua
@@ -0,0 +1,20 @@
+-------------------------------------------------------------------------------
+-- Module API
+-------------------------------------------------------------------------------
+
+function hc.reset_score.init()
+	hc.add_say_command('rs', hc.moderation.reset_score_command, hc.RESET_SCORE_LEVEL, nil,
+		'Reset your score, deaths and assists.', true)
+end
+
+-------------------------------------------------------------------------------
+-- Say commands
+-------------------------------------------------------------------------------
+
+function hc.moderation.reset_score_command(p, arg)
+	hc.exec(p, 'setscore ' .. p .. ' 0')
+	hc.exec(p, 'setdeaths ' .. p .. ' 0')
+	hc.exec(p, 'setassists ' .. p .. ' 0')
+
+	hc.info(p, 'Your score is now reset.')
+end
