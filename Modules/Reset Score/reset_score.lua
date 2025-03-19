edc936f831c143ceabd83598750ef9500badda8c
Mami-Tomoe <se7enclanonline@gmail.com>
2025-03-19 22:32:00 +0200

    Update reset_score.lua

--- Modules/Reset Score/reset_score.lua
+++ Modules/Reset Score/reset_score.lua
@@ -3,7 +3,7 @@
 -------------------------------------------------------------------------------
 
 function hc.reset_score.init()
-	hc.add_say_command('rs', hc.reset_score.reset_score_command, hc.RESET_SCORE_LEVEL, nil,
+	hc.add_say_command('rs', hc.moderation.reset_score_command, hc.RESET_SCORE_LEVEL, '',
 		'Reset your score, deaths and assists.', true)
 end
 
@@ -11,10 +11,20 @@ end
 -- Say commands
 -------------------------------------------------------------------------------
 
-function hc.reset_score.reset_score_command(p, arg)
+function hc.moderation.reset_score_command(p, arg)
+	if hc.undo then
+		local score, deaths, assists = player(p, 'score'), player(p, 'deaths'), player(p, 'assists')
+
+		hc.undo.register_command(p, 'rs', 'Restore your previous score.', function(p)
+			hc.exec(p, 'setscore ' .. p .. ' ' .. score)
+			hc.exec(p, 'setdeaths ' .. p .. ' ' .. deaths)
+			hc.exec(p, 'setassists ' .. p .. ' ' .. assists)
+		end)
+	end
+
 	hc.exec(p, 'setscore ' .. p .. ' 0')
 	hc.exec(p, 'setdeaths ' .. p .. ' 0')
 	hc.exec(p, 'setassists ' .. p .. ' 0')
 
-	hc.info(p, 'Your score is now reset.')
+	hc.info(p, 'K/D/A reset.')
 end
